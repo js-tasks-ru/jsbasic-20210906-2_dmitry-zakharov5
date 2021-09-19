9a2263c85aa94263121dda5641314d66f9376497
sasukeuchiwa666 <zakharovda95@gmail.com>
2021-09-19 19:12:27 +0300

    задачи второго модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,18 @@
 let calculator = {
-  // ваш код
+  firstValue: null,
+  secondValue: null,
+  read: function(a, b) {
+    calculator.firstValue = a;
+    calculator.secondValue = b;
+  },
+
+  sum: function() {
+    return this.firstValue + this.secondValue; 
+  },
+
+  mul: function() {
+    return this.firstValue * this.secondValue;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
