d677f922b048a937189d5a2dab63e08f4dfba9ab
sasukeuchiwa666 <zakharovda95@gmail.com>
2021-09-20 23:47:37 +0300

    исправил calculator на this

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,20 @@
 let calculator = {
   // ваш код
+  firstValue: null,
+  secondValue: null,
+  
+  read: function(a, b) {
+    this.firstValue = a;
+    this.secondValue = b;
+      },
+
+  sum: function() {
+    return this.firstValue + this.secondValue; 
+  },
+
+  mul: function() {
+    return this.firstValue * this.secondValue;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
