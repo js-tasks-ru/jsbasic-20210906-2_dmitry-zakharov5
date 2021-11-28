e3ff3b9fd8ac0e4b801b854fb3093c372588bd10
sasukeuchiwa666 <zakharovda95@gmail.com>
2021-11-28 13:33:14 +0300

    задание 8.2

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,70 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.template = this.createTemplate();
+    this.productsInner = this.template.querySelector('.products-grid__inner');
+    this.renderCards(this.products);
   }
+
+  createTemplate() {
+    return createElement(`
+    <div class="products-grid">
+      <div class="products-grid__inner"> 
+      </div>
+    </div>
+`);
+  }
+
+  get elem() {
+    return this.template;
+  }
+
+  renderCards(cards) {
+    for (let product of cards) {
+      let card = new ProductCard(product);
+      this.productsInner.append(card.elem);
+    }
+  }
+
+  clearProductsInner() {
+    while (this.productsInner.firstChild) {
+      this.productsInner.removeChild(this.productsInner.firstChild);
+    }
+  }
+
+  filterProducts(array, filter) {
+
+    const sortingByCategory = array.filter((product) => {
+
+      if (filter.noNuts) {
+        return !product.hasOwnProperty('nuts');
+      }
+            
+      if (filter.vegeterianOnly) {
+        return product.vegeterian;
+          
+      }
+            
+      if (filter.maxSpiciness < 4) {
+        return product.spiciness <= filter.maxSpiciness; 
+      }
+            
+      if (filter.category !== '') {
+        return product.category === filter.category;
+      }
+
+    });
+    return sortingByCategory;
+  }
+
+
+  updateFilter(filters) {
+    this.filters = {...this.filters, ...filters };
+    this.products = this.filterProducts(this.products, filters);
+    this.clearProductsInner(); 
+    this.renderCards(this.products);
+  }
+   
 }
+
+
